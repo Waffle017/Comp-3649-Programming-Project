3a44dd276530e4b6a17b3b5d03b92cb6076b82a1
Gabriel <grobl407@mtroyal.ca>
2026-04-10 21:40:54 -0600

    [PP-16] SCANNERRRRRRR

--- Haskell Solution/Scanner.hs
+++ Haskell Solution/Scanner.hs
@@ -1,10 +1,26 @@
 module Scanner 
 (
-
+    Token(..),
+    scanner
 )
 
 
 
 where
+    
+import Data.Char (isDigit)
 
+data Token = VarToken String | NumToken Int | LiveToken | OpToken String
+    deriving (Show)
 
+-- Function that takes the content of the file and converts each into a token
+scanner :: String -> [Token]
+scanner content = map wordToToken (filter (/= "=") (concatMap words (lines content)))
+
+-- Helper function that takes a single string (eg. "a") and decides which token constructor to put it in
+wordToToken :: String -> Token
+wordToToken str | str `elem` ["+", "-", "*", "/"] = OpToken str
+                | str == "live:" = LiveToken
+                | all isDigit str = NumToken (read str :: Int) 
+                | otherwise  = VarToken str
+                
